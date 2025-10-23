dcde864a71be8936053639b46ee2355d045c7aaa
Maksym Borovichenko <maxboraod@gmail.com>
2025-10-23 13:52:07 +0200

    add endpoint for product ranking

---
+++ LaptopsApi.Application/Queries/GetProductRatingQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace LaptopsApi.Application.Queries
+{
+    public sealed class GetProductRatingQuery : IRequest<double?>
+    {
+        public Guid ProductId { get; init; }
+    }
+}
